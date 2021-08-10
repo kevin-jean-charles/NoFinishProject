9db1aae7a9522dd0286f28567fe021add85fdd5f
gaelle1701 <gaellecoue@gmail.com>
2021-08-10 15:12:04 +0200

    add jwtUtils

--- ldvelh/src/main/java/com/nofinish/ldvelh/security/jwt/JwtUtils.java
+++ ldvelh/src/main/java/com/nofinish/ldvelh/security/jwt/JwtUtils.java
@@ -28,6 +28,7 @@ public class JwtUtils {
 
         return Jwts.builder()
                 .setSubject((userPrincipal.getUsername()))
+                .claim("id", userPrincipal.getId())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
